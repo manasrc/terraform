b8eb815ce4c24089f1f2a4912a7f1833abb50fb4
o0th <o0th@pm.me>
2024-06-26 19:45:28 +0200

    feat: digitalocean_kubernetes_cluster.development

---
+++ digitalocean_kubernetes_cluster.tf
@@ -0,0 +1,23 @@
+resource "digitalocean_kubernetes_cluster" "development" {
+  name = "development"
+
+  region   = var.do_region
+  vpc_uuid = digitalocean_vpc.development.id
+
+  auto_upgrade         = true
+  registry_integration = true
+
+
+  version = "1.30.1-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-1vcpu-2gb"
+    node_count = 3
+  }
+
+  maintenance_policy {
+    start_time = "00:00"
+    day        = "sunday"
+  }
+}
